8fa4ac148dd7bca062f6e7609acf308531bf3ef2
alaposi <agnes.laposi@gmail.com>
2019-09-13 18:19:25 +0200

    ConditionalVariableMutation is done

--- week-01/day-4/JavaBasics/src/ConditionalVariableMutations.java
+++ week-01/day-4/JavaBasics/src/ConditionalVariableMutations.java
@@ -1,6 +1,58 @@
 public class ConditionalVariableMutations {
     public static void main(String[] args) {
+        double a = 24;
+        int out = 0;
 
+        if (a % 2 == 0) {
+            out++;
+        }
+        System.out.println(out);
+
+
+        int b = 15;
+        String out2 = "";
+
+        if ((10 < b) && (b < 20)) {
+            out2 = "Sweet!";
+        } else if (b < 10) {
+            out2 = "Less!";
+        } else if (b > 20) {
+            out2 = "More!";
+        }
+        System.out.println(out2);
+
+
+        int c = 123;
+        int credits = 30;
+        boolean isBonus = false;
+
+        if (credits >= 50 && isBonus == false) {
+            c += 2;
+        } else if (credits < 50 && isBonus == false) {
+            c -= 1;
+        } else if (isBonus == true) {
+            c = c;
+        }
+
+        System.out.println(c);
+
+
+        int d = 8;
+        int time = 120;
+        String out3 = "";
+
+        if (d % 4 == 0 && time <= 120) {
+            out3 = "check";
+        } else if (time > 200) {
+            out3 = "Time out";
+        } else {
+            out3 = "Run Forest Run";
+        }
+
+        System.out.println(out3);
 
     }
+
+
 }
+

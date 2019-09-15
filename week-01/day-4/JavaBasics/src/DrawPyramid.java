2c2ff3571e518e0c9d7f040858ac5029407dad92
alaposi <agnes.laposi@gmail.com>
2019-09-15 16:21:55 +0200

    DrawPyramid is done

---
+++ week-01/day-4/JavaBasics/src/DrawPyramid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DrawPyramid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int lines = input.nextInt();
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 1; j < lines * 2; j++) {
+                if (j < (lines - i) || j > (lines + i)) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}

7e03627eb9222a0a772f639cdede3ad8f048eaff
alaposi <agnes.laposi@gmail.com>
2019-09-15 16:16:47 +0200

    PrintBigger updated

--- week-01/day-4/JavaBasics/src/PrintBigger.java
+++ week-01/day-4/JavaBasics/src/PrintBigger.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class PrintBigger {
     public static void main(String[] args) {
-        Scanner num1 = new Scanner(System.in);
-        Scanner num2 = new Scanner(System.in);
+        Scanner num = new Scanner(System.in);
         System.out.println("Enter 2 numbers");
         
-        double number1 = num1.nextDouble();
-        double number2 = num2.nextDouble();
+        double number1 = num.nextDouble();
+        double number2 = num.nextDouble();
 
         if (number1 > number2) {
             System.out.println(number1 + " is bigger");

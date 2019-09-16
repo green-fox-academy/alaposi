4e9d79c2ff567d00623fa7f9cd431d0ecc170e03
alaposi <agnes.laposi@gmail.com>
2019-09-16 20:49:34 +0200

    NumberSeqInEachCycle is done

---
+++ week-01/day-4/JavaBasics/src/NumberSequenceInEachCycle.java
@@ -0,0 +1,12 @@
+public class NumberSequenceInEachCycle {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            //int printed = i + 1;
+            for (int j = 0; j < i; j++) {
+                System.out.print((j + 1) + ", ");
+            }
+            System.out.println();
+        }
+    }
+}

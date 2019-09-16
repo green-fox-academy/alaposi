c4648d130fc0e1d51e61eb67e36ce0a191f23cf4
alaposi <agnes.laposi@gmail.com>
2019-09-16 20:55:42 +0200

    matrix modified

--- week-02/day-1/Arrays/src/Matrix.java
+++ week-02/day-1/Arrays/src/Matrix.java
@@ -1,6 +1,6 @@
 public class Matrix {
     public static void main(String[] args) {
-        int[][] matrix = new int[4][4];
+        int[][] matrix = new int[8][8];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 if (i == j) {

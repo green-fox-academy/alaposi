b6d1dbdb1c10091d74d23972e2ebb31b1a973adb
alaposi <agnes.laposi@gmail.com>
2019-09-20 11:33:37 +0200

    EnvelopStar is done

--- week-02/day-4/Drawing_Project/src/EnvelopStar.java
+++ week-02/day-4/Drawing_Project/src/EnvelopStar.java
@@ -7,14 +7,33 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class EnvelopStar {
     public static void mainDraw(Graphics graphics) {
 
+        int lineLength = WIDTH / 2;
+        int offset = lineLength / 16;
+        int[]center = {(WIDTH / 2), (HEIGHT / 2)};
+        //int[] center = {160, 160};
+        int x2 = WIDTH / 2;
+        int y2 = 0;
 
+        for (int i = 0; i < lineLength / offset; i++) {
+            graphics.setColor(Color.green);
+            graphics.drawLine(center[0] + i * offset, center[1], x2, y2 + i * offset);
+        }
+        for (int i = 0; i < lineLength / offset; i++) {
+            graphics.setColor(Color.green);
+            //change x1 offset
+            graphics.drawLine(center[0] - i * offset, center[1], x2, y2 + i * offset);
+        }
 
+        y2 = HEIGHT;
 
-
-
-
-
-
+        for (int i = 0; i < lineLength / offset; i++) {
+            graphics.setColor(Color.green);
+            graphics.drawLine(center[0] + i * offset, center[1], x2, y2 - i * offset);
+        }
+        for (int i = 0; i < lineLength / offset; i++) {
+            graphics.setColor(Color.green);
+            graphics.drawLine(center[0] - i * offset, center[1], x2, y2 - i * offset);
+        }
 
 
     }

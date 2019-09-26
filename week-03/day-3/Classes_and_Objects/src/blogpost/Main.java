3a3131a481f2f69ef84315f3ca3a776ecaa4ea84
alaposi <agnes.laposi@gmail.com>
2019-09-26 10:23:05 +0200

    task Blogspot is done

---
+++ week-03/day-3/Classes_and_Objects/src/blogpost/Main.java
@@ -0,0 +1,20 @@
+package blogpost;
+
+public class Main {
+  public static void main(String[] args) {
+
+    BlogPost lorem = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04");
+
+
+    BlogPost wait = new BlogPost();  // inkább a konstruktorban legyenek megadva az értékek!
+    wait.authorName = "Tim Urban";
+    wait.title = "Wait but why";
+    wait.text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
+    wait.publicationDate = "2010.10.10";
+
+
+    BlogPost engineer = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28");
+
+  }
+}
+

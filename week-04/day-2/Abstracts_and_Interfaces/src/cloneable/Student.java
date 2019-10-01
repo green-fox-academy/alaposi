14c2ec67e713dc4bb1842e3fecb9ec2ca6b98929
alaposi <agnes.laposi@gmail.com>
2019-10-01 15:16:15 +0200

    task Cloneable is done

---
+++ week-04/day-2/Abstracts_and_Interfaces/src/cloneable/Student.java
@@ -0,0 +1,49 @@
+package cloneable;
+
+import javafx.beans.binding.ObjectExpression;
+
+public class Student extends Person {
+  private String previousOrganization;
+  private int skippedDays;
+
+  @Override
+  public void getGoal() {
+    System.out.println("My goal is: Be a junior software developer.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + super.getName()+ ", a " + super.getAge() + " year old " + super.getGender() + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+  }
+
+//  public void clone(Student student){
+//
+//
+//  }
+
+  @Override
+  public Student clone(){
+    Student student = new Student(super.getName(), super.getAge(), super.getGender(), previousOrganization);
+    return student;
+  }
+
+
+  public void skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+  }
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public Student() {
+    super.setName("Jane Doe");
+    super.setAge(30);
+    super.setGender("female");
+    this.previousOrganization = "The school of life";
+    this.skippedDays = 0;
+
+  }
+}

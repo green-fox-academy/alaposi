8ede52a133cdbd430074df3f711608e66f2976f5
alaposi <agnes.laposi@gmail.com>
2019-10-21 15:24:35 +0200

    Exercise added

---
+++ week-06/day-1/Streams/src/Exercise1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise1 {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+    numbers.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+    
+
+    List<Integer> evenNumbers = new ArrayList<>();
+    numbers.stream().filter(x -> x % 2 == 0).forEach(x -> evenNumbers.add(x));
+  }
+}
+
+
+//  Write a Stream Expression to get the even numbers from the following list:
+//    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
